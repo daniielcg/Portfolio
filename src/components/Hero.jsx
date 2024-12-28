3d44578294ced2b838a282223e98b04640be8056
Daniel <danielcatgon04@gmail.com>
2024-12-28 16:16:10 +0000

    e

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <a
-              href="public\CV (Inglês).docx"  
+              href="/CV (Inglês).docx"  
               download="Daniel_Goncalves_CV.docx"
               className="px-1 inline-block py-1 w-full sm:w-auto rounded-full bg-gradient-to-br from-[#4b0082] to-[#8a2be2] hover:bg-slate-800 text-white mt-3 sm:ml-4"
             >
